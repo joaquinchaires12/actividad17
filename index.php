73e85cea47943da5c84c9aea1919289affd41ffc
joaquinchaires12 <160643978+joaquinchaires12@users.noreply.github.com>
2024-05-17 17:10:20 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php 
+    echo '<p>nombre: Joaquin Ruben Chaires Fernandez de Lara</p>';
+    echo '<p>Calle: azucenas</p>';
+    echo '<p>Numero de casa: 1209</p>';
+    echo '<p>colonia: esperanza</p>';
+    echo '<p>municipio: Guadalajara</p>';
+    echo '<p>telefono: 8441001010</p>';
+    ?> 
+
+</html>
